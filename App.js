7b9118284346997583104f5406ff608b86e117a9
Jason Cheng <jasoncheng224@gmail.com>
2021-02-19 18:34:08 -0800

    AsyncStorage util functions (also beware, I removed package-lock.json)

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import {StatusBar} from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
     return (
-        <View style={{padding: 50}}>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-                <TextInput placeholder={"Course Goal"} style={{width: '80%', borderBottomColor: 'black', borderWidth: 0.5, padding: 10}}/>
-                <Button title={"Add"}/>
+        <View style={{ padding: 50 }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <TextInput placeholder={"Course Goal"} style={{ width: '80%', borderBottomColor: 'black', borderWidth: 0.5, padding: 10 }} />
+                <Button title={"Add"} />
             </View>
             <View>
-                <Button title={"Add"}/>
-                <TextInput> Hello World</TextInput>
+                <Button title={"Save Data"} onPress={buttonClick} />
+                <TextInput>Hello World</TextInput>
             </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({});
+var k = true;
+function buttonClick() {
+    if (k) {
+        var r = Math.random();
+        storeData("key", r);
+        console.log(r);
+    } else {
+        (async () => {
+            alert(await getData("key"));
+        })()
+    }
+    k = !k;
+}
+
+// Stores the data; no need to worry about converting to strings ;)
+// (key needs to be string tho)
+const storeData = async (key, value) => {
+    try {
+        if (typeof value === "object") {
+            value = "json|" + JSON.stringify(value);
+        } else {
+            value = typeof value + "|" + value;
+        }
+        await AsyncStorage.setItem(key, value);
+    } catch (e) {
+        // saving error
+        console.log("storeData error: " + e.message);
+    }
+}
+
+// Gets the data; no need to worry about converting from strings ;)
+// (key needs to be string tho)
+const getData = async (key) => {
+    try {
+        var value = await AsyncStorage.getItem(key);
+        if (value !== null) {
+            // value previously stored
+            var type = value.split("|")[0];
+            value = value.substr(type.length + 1);
+            switch (type) {
+                case "json":
+                    value = JSON.parse(value);
+                    break;
+                case "boolean":
+                    value = value === "true";
+                    break;
+                case "number":
+                    value = Number(value);
+                    break;
+            }
+            return value;
+        }
+    } catch (e) {
+        // error reading value
+        console.log("getData error: " + e.message);
+    }
+}
