34273d6b0530238d2a33215f7c02e6a39b7bc8f0
Jason Cheng <jasoncheng224@gmail.com>
2021-02-21 23:13:04 -0800

    working video and very laggy posenet; nonworking skeleton

--- App.js
+++ App.js
@@ -1,96 +1,194 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { BottomNavigation, Drawer, Avatar, IconToggle, Toolbar } from "react-native-material-ui";
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, Text, View, ScrollView, StyleSheet, Button, Platform, Dimensions } from "react-native";
+import Constants from "expo-constants";
 
-class App extends React.Component {
+// camera
+import { Camera } from "expo-camera";
 
-    state = {
-        active: ""
+// tensorflow
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import { cameraWithTensors } from "@tensorflow/tfjs-react-native";
+
+// canvas
+import Canvas from "react-native-canvas";
+
+
+export default function App() {
+
+    //Tensorflow and Permissions
+    const [posenetModel, setPosenetModel] = useState(null);
+    const [frameworkReady, setFrameworkReady] = useState(false);
+
+    const TensorCamera = cameraWithTensors(Camera);
+    let requestAnimationFrameId = 0;
+
+    //performance hacks (Platform dependent)
+    const textureDims = { width: 1600, height: 1200 };
+    const tensorDims = { width: 152, height: 200 };
+
+    const [ctx, setCanvasContext] = useState(null);
+
+
+    //-----------------------------
+    // Run effect once
+    // 1. Check camera permissions
+    // 2. Initialize TensorFlow
+    // 3. Load Posenet Model
+    //-----------------------------
+    useEffect(() => {
+        if (!frameworkReady) {
+            (async () => {
+
+                //check permissions
+                const { status } = await Camera.requestPermissionsAsync();
+                console.log(`permissions status: ${status}`);
+
+                //we must always wait for the Tensorflow API to be ready before any TF operation...
+                await tf.ready();
+
+                //load the mobilenet model and save it in state
+                setPosenetModel(await posenet.load());
+                console.log(posenetModel);
+
+                setFrameworkReady(true);
+            })();
+        }
+    }, []);
+
+
+    //--------------------------
+    // Run onUnmount routine
+    // for cancelling animation 
+    // (if running) to avoid leaks
+    //--------------------------
+    useEffect(() => {
+        return () => {
+            cancelAnimationFrame(requestAnimationFrameId);
+        };
+    }, [requestAnimationFrameId]);
+
+
+    const getPrediction = async (tensor) => {
+        if (!tensor) return;
+
+        // TENSORFLOW MAGIC HAPPENS HERE!
+        const pose = await posenetModel.estimateSinglePose(tensor, 0.5, true, 16);
+        if (pose === null) return;
+        
+        drawSkeleton(pose);
     }
 
-    render() {
-        return (
-            <View>
-                <Toolbar
-                    leftElement="menu"
-                    centerElement="Searchable"
-                    searchable={{
-                        autoFocus: true,
-                        placeholder: 'Search',
-                    }}
-                    rightElement={{
-                        menu: {
-                            icon: "more-vert",
-                            labels: ["item 1", "item 2"]
-                        }
-                    }}
-                    onRightElementPress={(label) => { console.log(label) }}
-                    style={{
-                        fontSize: 2
-                    }}
-                />
 
-                <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-                    <TextInput placeholder={"Course Goal"} style={{ width: "80%", borderBottomColor: "black", borderWidth: 0.5, padding: 10 }} />
-                    <View>
-                        <IconToggle name="person" />
-                    </View>
-                </View>
-                <View>
-                    <Button title={"Save Data"} onPress={buttonClick} />
-                    <TextInput>Hello World</TextInput>
-                </View>
-                <BottomNavigation active={this.state.active} hidden={false} >
-                    <BottomNavigation.Action
-                        key="today"
-                        icon="today"
-                        label="Today"
-                        onPress={() => this.setState({ active: "today" })}
-                    />
-                    <BottomNavigation.Action
-                        key="people"
-                        icon="people"
-                        label="People"
-                        onPress={() => this.setState({ active: "people" })}
-                    />
-                    <BottomNavigation.Action
-                        key="bookmark-border"
-                        icon="bookmark-border"
-                        label="Bookmark"
-                        onPress={() => this.setState({ active: "bookmark-border" })}
-                    />
-                    <BottomNavigation.Action
-                        key="settings"
-                        icon="settings"
-                        label="Settings"
-                        onPress={() => this.setState({ active: "settings" })}
-                    />
-                </BottomNavigation>
+    const drawPoint = (x, y) => {
+        ctx.beginPath();
+        ctx.arc(x, y, 3, 0, 2 * Math.PI);
+        ctx.fillStyle = "#00ff00";
+        ctx.fill();
+        ctx.closePath();
+    }
 
 
+    const drawSegment = (x1, y1, x2, y2) => {
+        console.log(`${x1}, ${y1}, ${x2}, ${y2}`);
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#00ff00";
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+
+    const drawSkeleton = (pose) => {
+        const minPartConfidence = 0.1;
+        const adjacentKeyPoints = posenet.getAdjacentKeyPoints(pose.keypoints, minPartConfidence);
+        adjacentKeyPoints.forEach((keypoints) => {
+            drawSegment(keypoints[0].position.x, keypoints[0].position.y, keypoints[1].position.x, keypoints[1].position.y);
+        });
+    }
+
+
+    const handleCameraStream = (imageAsTensors) => {
+        const loop = async () => {
+            const nextImageTensor = await imageAsTensors.next().value;
+            await getPrediction(nextImageTensor);
+            requestAnimationFrameId = requestAnimationFrame(loop);
+        };
+        loop();
+    }
+
+
+    // https://js.tensorflow.org/api_react_native/0.2.1/#cameraWithTensors
+    const renderCameraView = () => {
+        return <View style={styles.cameraView}>
+            <TensorCamera
+                style={styles.camera}
+                type={Camera.Constants.Type.front}
+                zoom={0}
+                cameraTextureHeight={textureDims.height}
+                cameraTextureWidth={textureDims.width}
+                resizeHeight={tensorDims.height}
+                resizeWidth={tensorDims.width}
+                resizeDepth={3}
+                onReady={(imageAsTensors) => handleCameraStream(imageAsTensors)}
+                autorender={true}
+            />
+        </View>;
+    }
+
+
+    const handleCanvas = (canvas) => {
+        if (canvas === null) return;
+        const ctx = canvas.getContext("2d");
+        setCanvasContext(ctx);
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.body}>
+                {renderCameraView()}
+                <Canvas ref={handleCanvas} style={styles.canvas} />
             </View>
-        );
-    }
+        </View>
+    );
 }
 
-export default App;
+const CAM_WIDTH = Dimensions.get("window").width;
+const CAM_HEIGHT = CAM_WIDTH * 4 / 3;
 
-const styles = StyleSheet.create({});
-var k = true;
-function buttonClick() {
-    if (k) {
-        var r = Math.random();
-        storeData("key", r);
-        console.log(r);
-    } else {
-        (async () => {
-            alert(await getData("key"));
-        })()
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: "#E8E8E8"
+    },
+    body: {
+    },
+    cameraView: {
+        width: CAM_WIDTH,
+        height: CAM_HEIGHT
+    },
+    camera: {
+        width: "100%",
+        height: "100%",
+        zIndex: 1
+    },
+    canvas: {
+        width: CAM_WIDTH,
+        height: CAM_HEIGHT,
+        zIndex: 2,
+        position: "absolute"
     }
-    k = !k;
-}
+});
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 // Stores the data; no need to worry about converting to strings ;)
 // (key needs to be string tho)
